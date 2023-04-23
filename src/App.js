7873c51865f7c5d46f442eda82e25fbf6e0674e4
skenanoglu <kenanoglu115@gmail.com>
2023-04-23 22:42:43 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { Row, Form, Button, Col, Card } from 'antd';
+import { jsPDF } from "jspdf";
 
 function App() {
+
+  var onClick = () => {
+    var generateData = function (amount) {
+      var result = [];
+      var data = {
+        coin: "100",
+        game_group: "GameGroup",
+        game_name: "XPTO2",
+        game_version: "25",
+        machine: "20485861",
+        vlt: "0"
+      };
+      for (var i = 0; i < amount; i += 1) {
+        data.id = (i + 1).toString();
+        result.push(Object.assign({}, data));
+      }
+      return result;
+    };
+
+    function createHeaders(keys) {
+      var result = [];
+      for (var i = 0; i < keys.length; i += 1) {
+        result.push({
+          id: keys[i],
+          name: keys[i],
+          prompt: keys[i],
+          width: 65,
+          align: "center",
+          padding: 0
+        });
+      }
+      return result;
+    }
+
+    var headers = createHeaders([
+      "id",
+      "coin",
+      "game_group",
+      "game_name",
+      "game_version",
+      "machine",
+      "vlt"
+    ]);
+
+    var doc = new jsPDF({ putOnlyUsedFonts: true, orientation: "landscape" });
+    doc.table(1, 1, generateData(100), headers, { autoSize: true });
+    doc.save();
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card>
+      <Form>
+        <Row>
+          <Col>
+            <Button onClick={onClick}>Buton</Button>
+          </Col>
+        </Row>
+      </Form>
+    </Card>
+
   );
 }
 
